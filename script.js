9aa12daf3de790f6b4e2135b0a17acb0a22913ef
StephanyValarezo <valarezo@fiec.espol.edu.ec>
2018-11-30 11:10:38 -0500

    Pagina servicio v2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function addNew(titulo, contenido, imagen) {
-   var title = $("<h5/>", {
+   var title = $("<h2/>", {
       "class":"card-title",
       html: titulo
     })
@@ -11,19 +11,24 @@ function addNew(titulo, contenido, imagen) {
     })
 
     var im =$("<img/>",{
-      "src": imagen,
+      "src": imagen, "width":"700px", "height":"500px",
       
       html: "hola"
     })
 
     var div = $( "<div/>", {
-      "class": "card-body"
+      "class": "col-xs-12 col-sm12 col-md-4 col-lg-4 col-xl-4 text-center"
+    });
+
+    var div2 = $( "<div/>", {
+      "class":"col-xs-12 col-sm12 col-md-8 col-lg-8 col-xl-8"
     });
     
     title.appendTo(div)
     p.appendTo(div)
-    im.appendTo(div)
+    im.appendTo(div2)
     div.appendTo( "#noticias" );
+    div2.appendTo( "#noticias" );
 }
 
 
